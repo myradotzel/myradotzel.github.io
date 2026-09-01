74df7305319eb8e9eb68f4c933e2a6b16549dcb2
Myra Dotzel <myradotzel@gmail.com>
2026-08-31 22:18:56 -0400

    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -135,25 +135,32 @@ for (let i = 0; i < formInputs.length; i++) {
 }
 
 
-
 // page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
 
-// add event to all nav link
-for (let i = 0; i < navigationLinks.length; i++) {
-  navigationLinks[i].addEventListener("click", function () {
+// add event to all nav links
+for (const link of navigationLinks) {
+  link.addEventListener("click", function () {
 
-    for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
-        pages[i].classList.add("active");
-        navigationLinks[i].classList.add("active");
-        window.scrollTo(0, 0);
-      } else {
-        pages[i].classList.remove("active");
-        navigationLinks[i].classList.remove("active");
-      }
-    }
+    const selectedPage = this.innerHTML.toLowerCase();
 
+    // Activate the correct page
+    pages.forEach(page => {
+      page.classList.toggle(
+        "active",
+        page.dataset.page === selectedPage
+      );
+    });
+
+    // Activate the correct navigation button
+    navigationLinks.forEach(navLink => {
+      navLink.classList.toggle(
+        "active",
+        navLink === this
+      );
+    });
+
+    window.scrollTo(0, 0);
   });
 }
